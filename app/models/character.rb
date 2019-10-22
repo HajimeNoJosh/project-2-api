8eaf306b6bc13299d354b979b1c96bb0d3e571c8
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-10-22 15:58:45 -0400

    Updated model to capitalize names as users save them

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,12 @@
 class Character < ApplicationRecord
   belongs_to :user
   validates :given_name, :family_name, :user, presence: true
+
+  def given_name=(s)
+    write_attribute(:given_name, s.to_s.titleize)
+  end
+
+  def family_name=(s)
+    write_attribute(:family_name, s.to_s.titleize)
+  end
 end
